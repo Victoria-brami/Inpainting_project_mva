89ecfec43e2f98027cc0b3f2c1001152a0ccbec1
unknown <victoria.brami88@gmail.com>
2022-02-13 12:04:28 +0100

    updated metrics

--- code/evaluation_tools/evaluation_metrics.py
+++ code/evaluation_tools/evaluation_metrics.py
@@ -8,6 +8,7 @@ def get_mse(gt_loader, gen_loader, mask_list=None):
     with torch.no_grad():
         for idx, (gt_batch, gen_batch) in enumerate(zip(gt_loader, gen_loader)):
             batch_size = gt_batch.shape[0]
+            print()
             if batch_size != 0:
                 if type(gt_batch) == torch.Tensor or type(gen_batch) == torch.Tensor:
                     gt_batch = gt_batch.permute(0, 2, 3, 1).numpy()
@@ -31,11 +32,7 @@ def get_ssim(gt_loader, gen_loader, mask_loader):
                 for img_idx in range(batch_size):
                     im_gt = gt_batch[img_idx,:,:,:]
                     im_gen = gen_batch[img_idx,:,:,:]
-                    im_gt = im_gt[np.where(np.array(mask_loader[counter]) != 0)]
-                    im_gen = im_gen[np.where(np.array(mask_loader[counter]) != 0)]
-                    print(np.array(mask_loader[counter]).shape, im_gt.shape, im_gen.shape)
                     ssim_list.append(structural_similarity(im_gt, im_gen, multichannel=True))
-                    #ssim_list.append(structural_similarity(gt_batch[img_idx, :, :, :]*mask_loader[counter], gen_batch[img_idx, :, :, :]*mask_loader[counter, :, :], multichannel=True))
                     counter +=1
     print("SSIM Done")
     return np.mean(ssim_list)
